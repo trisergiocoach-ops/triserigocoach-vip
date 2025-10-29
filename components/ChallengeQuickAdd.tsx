af81c71fa1b47f1375e84c7a90f4f06421321696
trisergiocoach-ops <trisergiocoach@gmail.com>
2025-10-29 09:31:55 +0100

    Update ChallengeQuickAdd.tsx

--- components/ChallengeQuickAdd.tsx
+++ components/ChallengeQuickAdd.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { supabase } from "@/lib/supabaseClient";
+import { supabase } from "../lib/supabaseClient"; // <-- relativo
 import { useState } from "react";
 
 const RULES = [
